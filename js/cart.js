a0ccddea40e1a1530c4e90f9f273b83511688f9f
zero22813 <mmalyhin4@gmail.com>
2021-12-24 11:38:14 +0300

    cart

--- js/cart.js
+++ js/cart.js
@@ -1,60 +1,63 @@
+//data
 var items = JSON.parse('[{"name": "ASUS Laptop E210MA-GJ001T","cost": "19990", "img" : "img/10.webp"}, {"name": "Acer Aspire XC-830","cost": "19990", "img" : "img/3.webp"}]');
+
+
 function render_cart(){
-    var cart = document.querySelector("div.cart");
+    let cart = document.querySelector("div.cart");
 
     for (let i = 0; i < items.length; i++) {
         //item
-        var block = document.createElement("div");
+        let block = document.createElement("div");
         block.classList.add("item");
 
         //image
-        var image = document.createElement("div");
+        let image = document.createElement("div");
         image.classList.add("image");
-        var img = document.createElement("img");
+        let img = document.createElement("img");
         img.setAttribute("src", items[i].img);
         img.setAttribute("height", "120px");
         image.appendChild(img);
         block.appendChild(img);
 
         //desc
-        var desc = document.createElement("div");
+        let desc = document.createElement("div");
         desc.classList.add("description");
 
-        var dname = document.createElement("span");
+        let dname = document.createElement("span");
         dname.textContent = items[i].name;
         desc.appendChild(dname);
 
-        var dcost = document.createElement("span");
+        let dcost = document.createElement("span");
         dcost.setAttribute("id", "cost");
         dcost.textContent = items[i].cost;
         desc.appendChild(dcost);
         block.appendChild(desc);
 
         //qty
-        var qty = document.createElement("div");
+        let qty = document.createElement("div");
         qty.classList.add("quantity");
 
-        var btn_mns = document.createElement("button");
+        let btn_mns = document.createElement("button");
         btn_mns.classList.add("plus-btn");
         btn_mns.textContent = "-";
         btn_mns.setAttribute("onclick", "mns(" + i + ")");
         qty.appendChild(btn_mns);
 
-        var qtyinp = document.createElement("input");
+        let qtyinp = document.createElement("input");
         qtyinp.id = "qty";
         qtyinp.type = "text";
         qtyinp.value = 1;
         qtyinp.setAttribute("oninput", "num(" + i + ")");
         qty.appendChild(qtyinp);
 
-        var btn_pls = document.createElement("button");
+        let btn_pls = document.createElement("button");
         btn_pls.textContent = "+";
         btn_pls.setAttribute("onclick", "pls(" + i + ")");
         qty.appendChild(btn_pls);
 
-        var total_cost = document.createElement("div");
+        let total_cost = document.createElement("div");
         total_cost.classList.add("total-price");
-        var total_costtxt = document.createElement("span");
+        let total_costtxt = document.createElement("span");
         total_costtxt.id = "total-cost";
         total_costtxt.textContent = items[i].cost;
         total_cost.appendChild(total_costtxt);
@@ -63,9 +66,9 @@ function render_cart(){
         block.appendChild(qty);
 
         //btn-delete
-        var dlt_btn = document.createElement("div");
+        let dlt_btn = document.createElement("div");
         dlt_btn.classList.add("buttons");
-        var dlt_txt = document.createElement("span");
+        let dlt_txt = document.createElement("span");
         dlt_txt.classList.add("delete-btn");
         dlt_txt.textContent = "Удалить";
         dlt_btn.appendChild(dlt_txt);
